9b5f066167d544569444b702a4c0fd841d43ac00
Pavel Bobkov <p.bobkov@gmail.com>
2020-11-05 19:15:31 +0100

    OS views

---
+++ RunLiquidTest.rb
@@ -0,0 +1,19 @@
+
+require 'liquid'
+require 'json'
+
+#puts 'template_path, parameters_path ?'
+
+template_path = ARGV[0]
+
+parameters_path = ARGV[1]
+
+#template_path = gets.strip
+
+template_path = 'test.liquid'
+
+parameters_path = 'di_model.json'
+
+template = Liquid::Template.parse(File.read(template_path))
+
+puts template.render(JSON.parse(File.read(parameters_path)))
