6772ac391cee713d9e72c742fb2fb1fef8668783
Pavel Bobkov <p.bobkov@gmail.com>
2020-11-19 10:47:30 +0100

    custom input support

--- testing/RunLiquidTest.rb
+++ testing/RunLiquidTest.rb
@@ -10,10 +10,10 @@ parameters_path = ARGV[1]
 
 #template_path = gets.strip
 
-template_path = '../templates/generate_os.liquid'
+template_path = '../testing/update_custom.liquid'
 
-parameters_path = '../param_definitions/LDM_geo.json'
+parameters_path = './testing_model.json'
 
-template = Liquid::Template.parse(File.read(template_path))
+template = Liquid::Template.parse(File.read(template_path))	
 
 puts template.render(JSON.parse(File.read(parameters_path)))
