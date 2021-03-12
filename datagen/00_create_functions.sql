4b94311246c31e7e37aac89b32fa3ce59eb1163e
Pavel Bobkov <p.bobkov@gmail.com>
2021-03-12 13:16:31 +0100

    detach function creation to the separate file

---
+++ datagen/00_create_functions.sql
@@ -0,0 +1,38 @@
+---------------------------------------------------------------------
+---> Randomizing functions   
+---------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION randomdate (d1 TIMESTAMP, d2 TIMESTAMP) RETURN DATE
+AS
+BEGIN
+  RETURN TO_TIMESTAMP(EXTRACT(EPOCH FROM d1) + RANDOMINT(FLOOR(EXTRACT(EPOCH FROM d2) - EXTRACT(EPOCH FROM d1))::INT));
+END;
+
+CREATE OR REPLACE FUNCTION randomdata (id INT, field VARCHAR, cp BOOLEAN, geo VARCHAR) RETURN VARCHAR
+AS
+BEGIN
+   RETURN 
+        CASE 
+            WHEN geo = 'pushpin' THEN (1-(random()*2))::INT*40 || ';' || ((2-(random()*6))::INT*90) -- generating a random pushpin
+            WHEN cp is TRUE THEN id::INT || '_' || field -- when field is cp it will gereate within a limit for a dim table, default is 100
+            ELSE (random()*id)::INT || '_' || field -- for other attributes it's a variety of X values, default is 30 
+        END;
+END;
+
+CREATE OR REPLACE FUNCTION randomfact (minimum INT, maximum INT) RETURN INT
+AS
+BEGIN
+   RETURN (random()*ABS(maximum-minimum))::INT +
+        CASE WHEN maximum > minimum THEN minimum
+        ELSE maximum END;
+END;
+
+CREATE OR REPLACE FUNCTION randomcustomdata(values VARCHAR) RETURN VARCHAR
+AS
+BEGIN
+   RETURN 	
+   	CASE 
+		WHEN values LIKE '%,%' THEN TRIM(SPLIT_PART(values,',', (FLOOR(RANDOM() * (REGEXP_COUNT(values,',') + 1) + 1 ))::INT))
+		WHEN values LIKE '%-%' THEN randomfact(SPLIT_PART(values,'-',1)::INT, SPLIT_PART(values,'-',2)::INT)::VARCHAR
+	END;
+END;
+
