0b0160aa8d2d68fc6f4d63ab9d9b5e32474a6a16
djunGD <dan.jun@gooddata.com>
2021-03-16 11:18:07 -0500

    bug fix update

--- datagen/00_a_create_functions.sql
+++ datagen/00_a_create_functions.sql
@@ -25,3 +25,13 @@ BEGIN
         CASE WHEN maximum > minimum THEN minimum
         ELSE maximum END;
 END;
+
+CREATE OR REPLACE FUNCTION randomcustomdata(values VARCHAR) RETURN VARCHAR
+AS
+BEGIN
+   RETURN
+   	CASE
+		WHEN values LIKE '%,%' THEN TRIM(SPLIT_PART(values,',', (FLOOR(RANDOM() * (REGEXP_COUNT(values,',') + 1) + 1 ))::INT))
+		WHEN values LIKE '%-%' THEN randomfact(SPLIT_PART(values,'-',1)::INT, SPLIT_PART(values,'-',2)::INT)::VARCHAR
+	END;
+END;
